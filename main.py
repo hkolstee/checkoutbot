caebd8dfc490be42467d61e6d217e0467209347f
hkolstee <hkolstee@hotmail.com>
2021-11-26 18:01:28 +0100

    fails on buy

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium.webdriver import ChromeOptions, Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
+import time
+
 
 # detach option to allow window to stay open after script has ended
 opts = ChromeOptions()
@@ -11,15 +13,22 @@ opts.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options=opts)
 
+# Use the url of the shoe you want to buy
 # url = "https://www.nike.com/nl/launch/t/nike-sb-dunk-low-pro-emea"
 url = "https://www.nike.com/nl/launch/t/lahar-low-wheat-voor-dames"
-
 driver.get(url)
 
 # driver.maximize_window()
 
+# accept the cookies
 acceptCookies = driver.find_element(By.XPATH, '//*[@id="cookie-settings-layout"]/div/div/div/div[3]/div[1]/button')
 acceptCookies.click()
 
-# clickItem = driver.find_element('//*[@id="root"]/div/div/div[1]/div/div[1]/div[2]/div/section[1]/figure[25]/div/div/figcaption/div/div/div[2]/button')
-# clickItem.click()
+# select the size
+clickSize = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div/div[1]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/ul/li[11]/button')
+clickSize.click()
+
+# need workaround for buy button, fails like this
+clickBuy = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div/div[1]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/div/button')
+clickBuy.click()
+
