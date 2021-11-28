af3210047a190f6c444f7b610430535b9bb0529a
hkolstee <hkolstee@hotmail.com>
2021-11-28 22:01:07 +0100

    Need to make selenium undetectable

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import time
 import sys
+import wait
 
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, Chrome
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.action_chains import ActionChains
 
 def run(username, password, size, shoeURL = None):
 
     # detach option to allow window to stay open after script has ended
     opts = ChromeOptions()
+    # opts.headless = True
     opts.add_experimental_option("detach", True)
+    opts.add_experimental_option("useAutomationExtension", False)
+    opts.add_experimental_option("excludeSwitches",["enable-automation"])
+    # opts.add_argument("--allow-running-insecure-content") 
+    # opts.add_argument("--disable-web-security") 
+    # opts.add_argument("--allow-insecure-localhost")
 
     # Latest chrome driver
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options=opts)
@@ -19,6 +31,7 @@ def run(username, password, size, shoeURL = None):
     # Use given url
     driver.get(shoeURL)
 
+    driver.set_window_size(1920, 1080)
     # driver.maximize_window()
 
     acceptCookies(driver)
@@ -40,11 +53,33 @@ def acceptCookies(driver):
 def selectSize(driver, size):
     clickSize = driver.find_element(By.XPATH, "//*[text()='EU " + size + "']")
     clickSize.click()
+    driver.execute_script("arguments[0].click();", clickSize)
+
 
 # clicks the "Buy *price*" button, can't be done the simple way because the button is not interactable 
 def addToCart(driver):
-    clickBuy = driver.find_element(By.XPATH, "//*[contains(text(),'Buy ')]")
+    # Wait till buy button present
+    # clickBuy = WebDriverWait(driver, 1000, 0.01).until(ec.presence_of_element_located((By.XPATH, "//*[contains(text(),'Buy ')]")))
+    # clickBuy = WebDriverWait(driver, 1000, 0.01).until(ec.element_to_be_clickable((By.XPATH, "//*[contains(text(),'Buy ')]")))
+    # clickBuy = driver.find_element(By.XPATH, "//*[contains(text(),'Buy ')]")
+    clickBuy = driver.find_element(By.XPATH, """//*[@id="root"]/div/div/div[1]/div/div[1]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/div/button""")
+
+    driver.execute_script("arguments[0].scrollIntoView(true);", clickBuy)
+
+
+    # clickBuy.send_keys(Keys.ENTER)
+    # clickBuy.click()
+
+    # ActionChains(driver).move_to_element(clickBuy).click(clickBuy).perform()
+
+    # WebDriverWait(driver, 1000, 0.01).until(ec.element_to_be_clickable((By.XPATH, """//*[@id="root"]/div/div/div[1]/div/div[1]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/div/button""")))
+
+    # Make selenium run a javascript click on the element
+    clickBuy.click()
     driver.execute_script("arguments[0].click();", clickBuy)
+
+def waitForPageToLoad(TimeOutInSec):
+    pass
 
 def login(driver, username, password):
     pass
@@ -53,10 +88,10 @@ def login(driver, username, password):
 if __name__ == "__main__":
     # Count arguments, replace with try except blocks in future
     if ((len(sys.argv)-1) != 4):
-        print('\033[95m' + "----------------------------------------------------------")
+        print('\033[95m' + "--------------------------------------------------------------")
         print('\033[91m' + "To run script, use:")
         print('\033[93m' + "python3 main.py *username* *password* *size(EU)* *URL of shoe*")
-        print('\033[95m' + "----------------------------------------------------------" + '\u001b[0m')
+        print('\033[95m' + "--------------------------------------------------------------" + '\u001b[0m')
         exit()
     
     print('\033[92m' + "\nData entered" + 
